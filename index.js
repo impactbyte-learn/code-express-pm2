0e5930cee4fd4f4d479af8d01585134b4c149760
M Haidar Hanif <git@mhaidarhanif.com>
2018-09-03 12:53:32 +0700

    Create basic Express app

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send({
+  message: 'Hello World!'
+}))
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
